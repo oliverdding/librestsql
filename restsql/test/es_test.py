0f594a31de689fb99d576539da7511f1e80505fe
xiaozhang-123 <3499656821@qq.com>
2021-07-31 20:21:00 +0800

    7-31 结构已由字典转化为类 初步实现 druid查询（使用原生sql语句） 整合其他数据源，交接结构一致

--- restsql/test/es_test.py
+++ restsql/test/es_test.py
@@ -1,38 +1,38 @@
-# encoding=utf-8
-from restsql.restClient import restClient
-from restsql.config.settings import *
-if __name__ == '__main__':
-    query_dict = {
-            "from": "es_test.cars",
-            "time": {
-                "column": 'sold',
-                "begin": "",
-                "end": "",
-                "interval": "1M"
-            },
-            "select": [
-                {
-                    "column": "price",
-                    "alias": "price_sum",
-                    "metric": "sum"
-                }
-            ],
-            "where": [
-                {
-                    "column": "color",
-                    "op": "=",
-                    "value": "red"
-                }
-            ],
-            "group": []
-        }
-    db_configs.put(
-        DataBase(
-            name='es_test',
-            db_type=EnumDataBase.ES,
-            host='127.0.0.1',
-            port=9200
-        )
-    )
-    client=restClient(query_dict)
-    print(client.query())
+# # encoding=utf-8
+# from restsql.restClient import restClient
+# from restsql.config.settings import *
+# if __name__ == '__main__':
+#     query_dict = {
+#             "from": "es_test.cars",
+#             "time": {
+#                 "column": 'sold',
+#                 "begin": "",
+#                 "end": "",
+#                 "interval": "1M"
+#             },
+#             "select": [
+#                 {
+#                     "column": "price",
+#                     "alias": "price_sum",
+#                     "metric": "sum"
+#                 }
+#             ],
+#             "where": [
+#                 {
+#                     "column": "color",
+#                     "op": "=",
+#                     "value": "red"
+#                 }
+#             ],
+#             "group": []
+#         }
+#     db_configs.put(
+#         DataBase(
+#             name='es_test',
+#             db_type=EnumDataBase.ES,
+#             host='127.0.0.1',
+#             port=9200
+#         )
+#     )
+#     client=restClient(query_dict)
+#     print(client.query())
